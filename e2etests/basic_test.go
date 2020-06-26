9d079ffc641f066620040da791f46f2619b98480
Nick Loginov <nick.loginov@booking.com>
2020-06-26 10:32:57 +0200

    e2etests refactoring&improvements (#26)
    * e2etests refactoring&improvements
    
    1. Refactoring: separate world & fs services to separate files
    2. Improvement: ability to expect event bpfink writes to log
    3. Improvement: FS services to create/modify/delete files in easy way
    4. Improvement: Basic test now tests we catch events for
    creation/modification/deletion of generic file with expected structure
    
    * satisfy linter

--- e2etests/basic_test.go
+++ e2etests/basic_test.go
@@ -3,61 +3,34 @@
 package e2etests
 
 import (
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"os/signal"
-	"path/filepath"
-	"syscall"
+	"path"
 	"testing"
 )
 
-type World struct {
-	BPFink *BPFinkInstance
-	t      *testing.T
+func TestBPfink(t *testing.T) {
+	world := SetUp(t)
+	defer world.TearDown()
+	t.Run("generic file create/modify/delete", world.SubTest(testCreateGenericFile))
+
 }
 
-var (
-	bpfinkBinPath = flag.String("bpfink-bin", "", "ABSOLUTE path to tested bpfink binary")                             //nolint:gochecknoglobals
-	ebpfObjPath   = flag.String("ebpf-obj", "", "ABSOLUTE path to tested ebpf program")                                //nolint:gochecknoglobals
-	wait          = flag.Bool("wait", false, "set up environment & wait for user permission to run actual test suite") //nolint:gochecknoglobals
-)
-
-func SetUp(t *testing.T) *World {
-	if *wait && !testing.Verbose() {
-		t.Fatal("--wait option works only with -v (verbose) testing mode")
-	}
-
-	testRootDir, err := ioutil.TempDir("", "bpfink_test")
-	if err != nil {
-		t.Fatal("Can't create temp library for bpfink: ", err)
-	}
-
-	bpfink := BPFinkRun(t, BPFinkRunParameters{
-		BPFinkBinPath:      *bpfinkBinPath,
-		BPFinkEbpfProgramm: *ebpfObjPath,
-		SandboxDir:         filepath.Join(testRootDir),
+func testCreateGenericFile(t *testing.T, w *World) {
+	genericFile := path.Join(w.FS.GenericMonitoringDir, "hohoho.txt")
+	f := w.FS.MustCreateFile(t, genericFile)
+	w.BPFink.ExpectGenericEvent(t, Event{
+		File:    genericFile,
+		Message: "generic file created",
 	})
 
-	if *wait {
-		fmt.Fprintf(os.Stderr, "bpfink is running with %d pid. Press Ctrl+C to run actual test...", bpfink.cmd.Process.Pid)
-		resume := make(chan os.Signal, 1)
-		signal.Notify(resume, syscall.SIGINT)
-		<-resume
-	}
+	f.WriteString("hello world")
+	w.BPFink.ExpectGenericEvent(t, Event{
+		File:    genericFile,
+		Message: "generic file Modified",
+	})
 
-	return &World{
-		BPFink: bpfink,
-		t:      t,
-	}
-}
-
-func TearDown(w *World) {
-	w.BPFink.Shutdown()
-}
-
-func TestStartRespondDie(t *testing.T) {
-	world := SetUp(t)
-	defer TearDown(world)
+	w.FS.MustRemoveFile(t, genericFile)
+	w.BPFink.ExpectGenericEvent(t, Event{
+		File:    genericFile,
+		Message: "generic file deleted",
+	})
 }
